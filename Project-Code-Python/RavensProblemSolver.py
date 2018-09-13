4f78247071aff696b114939d3fa0384919ca33e2
Allan Reyes <reyallan@gatech.edu>
2018-09-13 15:54:23 -0700

    Apply all transformations for all axes
    This changes the solver logic to do the following:
    - Always apply all transformations both row-wise and column-wise
    - The solver applies each transformation and generates an answer
    - All the answers are sorted based on their similarities
    - The most similar answer is picked as the response
    - If none of the transformations found an appropriate answer, then
      the problem is skipped (-1)
    
    This change was able to solve the issue with the incorrect test case
    and now the agent correctly solves 8/12 for both Basic and Test B!

--- Project-Code-Python/RavensProblemSolver.py
+++ Project-Code-Python/RavensProblemSolver.py
@@ -1,11 +1,12 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 from collections import namedtuple
+from operator import itemgetter
 
 import numpy as np
 
 from RavensTransformation import FlipTransformation, MirrorTransformation, NoOpTransformation
 
-Transform = namedtuple('Transform', ['transformation', 'axis'])
+Answer = namedtuple('Answer', ['similarity', 'answer'])
 
 
 class RavensProblemSolverFactory:
@@ -43,15 +44,16 @@ class RavensProblemSolver:
         :return: The index of the selected answer.
         :rtype: int
         """
-        for transform in self._transforms:
-            transformation, axis = transform
+        # Apply each transformation for each axis (row=0, column=1)
+        answers = [self._apply(problem, transformation, axis) for axis in [0, 1] for transformation in self._transforms]
+        # Sort them in increasing order by their similarity measure
+        answers.sort(key=itemgetter(0), reverse=True)
 
-            matches = self._apply(problem.matrix, transformation, axis)
+        # The best answer is the first sorted element
+        _, best = answers[0]
 
-            if matches:
-                return self._find_answer(problem.matrix, problem.answers, transformation, axis)
-
-        return -1
+        # If no answer was found by any of the transformations, then skip the problem (-1), else return the best answer
+        return -1 if best is None else best
 
     def _are_similar(self, image, other_image):
         return self._similarity(image, other_image) >= self._SIMILARITY_THRESHOLD
@@ -76,15 +78,10 @@ class RavensProblemSolver:
         pass
 
     @abstractmethod
-    def _apply(self, matrix, transformation, axis):
+    def _apply(self, problem, transformation, axis):
         # Applies a particular transformation to the Raven's matrix of images for the given axis
-        # where axis = 0 means row-wise and axis = 1 means column-wise, and returns a boolean
-        # indicating whether the transformation matches or not
-        pass
-
-    @abstractmethod
-    def _find_answer(self, matrix, answers, transformation, axis):
-        # Finds the answer from the list of available candidates
+        # where axis = 0 means row-wise and axis = 1 means column-wise, and returns the best
+        # answer along with its similarity measure; if no answer is found, then `None` is returned
         pass
 
 
@@ -92,28 +89,29 @@ class _Ravens2x2Solver(RavensProblemSolver):
     @property
     def _transforms(self):
         return [
-            Transform(NoOpTransformation(), 0),
-            Transform(NoOpTransformation(), 1),
-            Transform(MirrorTransformation(), 0),
-            Transform(MirrorTransformation(), 1),
-            Transform(FlipTransformation(), 1)
+            NoOpTransformation(),
+            MirrorTransformation(),
+            FlipTransformation()
         ]
 
-    def _apply(self, matrix, transformation, axis):
+    def _apply(self, problem, transformation, axis):
+        matrix = problem.matrix
+        answers = problem.answers
+
         # The 'A' image
         image_1 = transformation.apply(matrix[0][0])
         # Either the 'B' image for row-wise or the 'C' image for column-wise
         image_2 = matrix[0][1] if axis == 0 else matrix[1][0]
 
-        # Compare the image with its pair to see if they match or not
-        return super(_Ravens2x2Solver, self)._are_similar(image_1, image_2)
+        # Compare the image with its pair, if they don't match then their is no answer
+        if not super(_Ravens2x2Solver, self)._are_similar(image_1, image_2):
+            return Answer(similarity=0., answer=None)
 
-    def _find_answer(self, matrix, answers, transformation, axis):
-        # The 'C' image for row-wise or the 'B' image for column-wise; this image is the last one and
-        # its transformation should be matched to one of the answers
-        image = transformation.apply(matrix[-1][0]) if axis == 0 else transformation.apply(matrix[0][-1])
         # Find the answer that most closely matches the transformed image, i.e. generate and test
-        candidate = np.argmax([super(_Ravens2x2Solver, self)._similarity(image, answer) for answer in answers])
+        # The 'C' image for row-wise or the 'B' image for column-wise
+        last_image = transformation.apply(matrix[1][0]) if axis == 0 else transformation.apply(matrix[0][1])
+        similarities = [super(_Ravens2x2Solver, self)._similarity(last_image, answer) for answer in answers]
+        candidate = np.argmax(similarities)
 
         # Answers are one-indexed based
-        return candidate + 1
+        return Answer(similarity=similarities[candidate], answer=candidate + 1)
