b735e6aa329372a40e234f9b500b466b3527e288
Allan Reyes <reyallan@gatech.edu>
2018-10-21 14:01:03 -0700

    Optimize implementation of RavensFractalEncoder
    - Add a cache layer for fractals
    - Decompose the image into blocks using Numpy's stride tricks
    - Vectorize transformation to act on all blocks at the same time
    - Terminate the search when finding candidate with correspondence == 0
    - Do not encode blocks that are all white. We will assume the image
      always has a white background so we only need to care about the
      black portions which correspond to shapes
    
    This optimizes the encoder's runtime from 3 minutes to less than 100 ms!

--- Project-Code-Python/RavensFractal.py
+++ Project-Code-Python/RavensFractal.py
@@ -1,9 +1,11 @@
+import time
 from collections import namedtuple
 
 import numpy as np
+from numpy.lib import stride_tricks
 
 _FractalTransformation = namedtuple('FractalTransformation', ['name', 'transform'])
-_FractalCandidate = namedtuple('FractalCandidate', ['rmse', 'fractal'])
+_FractalCandidate = namedtuple('FractalCandidate', ['correspondence', 'fractal'])
 
 
 class RavensMutualFractalFactory:
@@ -30,6 +32,7 @@ class RavensMutualFractalFactory:
 
             fractals = [fractals_ab, fractals_ba]
         elif len(images) == 3:
+            # TODO: Fix implementation (see notes in phone)!
             fractals_ij = self._encoder.apply(images[0], images[1], size)
             fractals_jk = self._encoder.apply(images[1], images[2], size)
             fractals_ik = self._encoder.apply(images[0], images[2], size)
@@ -60,26 +63,64 @@ class RavensMutualFractal:
     def features(self):
         return self._features
 
+    def intersection(self, other):
+        """
+        Intersects this MutualFractal's set of features with another MutualFractal.
+
+        :param other: The other MutualFractal.
+        :type other: RavensMutualFractal
+        :return: Intersection between the features.
+        :rtype: set
+        """
+        return self._features.intersection(other.features)
+
+    def __sub__(self, other):
+        """
+        Computes the difference between this MutualFractal's features and another MutualFractal.
+
+        :param other: The other MutualFractal.
+        :type other: RavensMutualFractal
+        :return: The difference between the features.
+        :rtype: set
+        """
+        return self._features - other.features
+
 
 class RavensFractalEncoder:
     """
     Implements the fractal encoding algorithm as defined by McGreggor, Kunda and Goel in
-    "A Fractal Approach Towards Visual Analogy".
+    "A Fractal Approach Towards Visual Analogy", and as described by McGreggor in his
+    doctoral dissertation "Fractal Reasoning".
 
-    Reference: http://dilab.gatech.edu/publications/McGreggorKundaGoel_ICCCX_2010.pdf
+    This implementation contains some optimization tricks to speed up its computation.
+
+    References:
+        - http://dilab.gatech.edu/publications/McGreggorKundaGoel_ICCCX_2010.pdf
+        - https://smartech.gatech.edu/bitstream/handle/1853/50337/MCGREGGOR-DISSERTATION-2013.pdf
     """
 
+    # The weights to use in the correspondence computation chosen after empirical experimentation
+    _PHOTOMETRIC_WEIGHT = 0.8
+    _DISTANCE_WEIGHT = 0.2
+
     def __init__(self):
-        # The list of affine transformations to apply
+        # The list of affine transformations to apply as described in McGreggor's dissertation
+        # This transformations are vectorized to work with a stack of `size x size` blocks
+        # Reference:
+        # https://stackoverflow.com/a/43864937
         self._transformations = [
             _FractalTransformation(name='Identity', transform=lambda x: x),
-            _FractalTransformation(name='Horizontal Reflection', transform=lambda x: np.fliplr(x)),
-            _FractalTransformation(name='Vertical Reflection', transform=lambda x: np.flipud(x)),
-            _FractalTransformation(name='Rotation 90', transform=lambda x: np.rot90(x)),
-            _FractalTransformation(name='Rotation 180', transform=lambda x: np.rot90(x, k=2)),
-            _FractalTransformation(name='Rotation 270', transform=lambda x: np.rot90(x, k=3)),
-            _FractalTransformation(name='Color Shift', transform=lambda x: 255 - x)
+            _FractalTransformation(name='Horizontal Reflection', transform=lambda x: x[..., ::-1]),
+            _FractalTransformation(name='Vertical Reflection', transform=lambda x: x[..., ::-1, :]),
+            _FractalTransformation(name='Rotation 90', transform=lambda x: x.swapaxes(-2, -1)[..., ::-1, :]),
+            _FractalTransformation(name='Rotation 180', transform=lambda x: x[..., ::-1, ::-1]),
+            _FractalTransformation(name='Rotation 270', transform=lambda x: x.swapaxes(-2, -1)[..., ::-1]),
+            _FractalTransformation(name='Reflection YnX', transform=lambda x: x.swapaxes(-2, -1)[..., ::-1, ::-1]),
+            _FractalTransformation(name='Reflection YX', transform=lambda x: x.swapaxes(-2, -1))
         ]
+
+        # A cache for candidate fractals
+        self._cached_candidates = {}
 
     def apply(self, source, destination, size):
         """
@@ -91,93 +132,123 @@ class RavensFractalEncoder:
         :type destination: ndarray
         :param size: The size of the representation.
         :type size: int
-        :return: A set of fractal features derived from the fractal encoding.
-        :rtype: set
+        :return: A tuple with the list of fractals and a set of fractal features derived from the fractal encoding.
+        :rtype: tuple
         """
         assert source.shape == destination.shape, 'Both source and destination must have the same shape!'
 
+        # The cache is only valid per run
+        self._cached_candidates.clear()
+
         # Decompose the destination and source into a set of N smaller images of size `size x size`
-        N = destination.shape[0] // size
-        destination_index = [(col * size, row * size) for row in range(0, N) for col in range(0, N)]
-        source_index = destination_index[:]
+        # This will be done in a sliding window fashion with a step size, or stride, of 1 and blocks of `size x size`
+        N = destination.shape[0] - size + 1
+        block_indices = [(row, col) for row in range(0, N) for col in range(0, N)]
 
-        fractals = []
+        source_blocks = self._decompose(source, size)
+        destination_blocks = self._decompose(destination, size)
 
-        # For each block in the destination
-        for d_index in destination_index:
-            # Retrieve the corresponding block from the destination image
-            destination_image = destination[d_index[0]:d_index[0] + size, d_index[1]:d_index[1] + size]
-            # Find the best candidate that approaches the destination image given the source image
-            best_candidate = self._find_best_candidate(destination_image, d_index, source, source_index, size)
+        # For each block in the destination, find the best candidate that approaches the destination given the source
+        # As an optimization, skip blocks that are completely white as we will assume the image always has a white
+        # background, and thus we will only be encoding the black portions which correspond to shapes
+        fractals = [
+            self._find_best_candidate(destination_blocks[i], i, source_blocks, block_indices).fractal
+            for i in range(0, len(block_indices))
+            if not np.alltrue(destination_blocks[i] == 255)
+        ]
 
-            fractals.append(best_candidate.fractal)
+        return fractals, self._to_features(fractals)
 
-        # Make sure the destination image was encoded correctly.
-        # The number of fractals should be the same as the number of blocks the destination image was decomposed into
-        assert len(fractals) == len(destination_index), 'Destination image was not decomposed correctly!'
-
-        return self._to_features(fractals)
-
-    def _find_best_candidate(self, destination_image, destination_image_index, source, source_index, size):
+    def _find_best_candidate(self, destination_block, destination_index, source_blocks, block_indices):
         # Examine the entire source image for an equivalent block such that an affine transformation
         # will result in the destination block
+
+        cached_candidate = self._get_cached_candidate(destination_block)
+
+        if cached_candidate is not None:
+            return _FractalCandidate(correspondence=cached_candidate[0], fractal=(cached_candidate[1],
+                                                                                  block_indices[destination_index],
+                                                                                  cached_candidate[2]))
+
         best_candidate = None
+        best_found = False
 
-        for s_index in source_index:
-            # Retrieve the corresponding block from the source image
-            source_image = source[s_index[0]:s_index[0] + size, s_index[1]:s_index[1] + size]
+        for transformation in self._transformations:
+            # Transform all blocks at the same time using vectorization
+            transformed_blocks = transformation.transform(source_blocks)
 
-            for transformation in self._transformations:
-                transformed = transformation.transform(source_image)
-                rmse = self._similarity(destination_image.astype(int), transformed.astype(int))
+            for s_index, s_block in enumerate(transformed_blocks):
+                correspondence = self._correspondence(destination_block, s_block)
 
-                if not best_candidate or rmse > best_candidate.rmse:
+                if not best_candidate or correspondence < best_candidate.correspondence:
                     # Only keep the best candidate
+                    best_candidate = _FractalCandidate(correspondence=correspondence,
+                                                       fractal=(block_indices[s_index],
+                                                                block_indices[destination_index],
+                                                                transformation.name))
 
-                    # The location of the leftmost and topmost pixel in the source image
-                    sx, sy = s_index
-                    # The location of the leftmost and topmost pixel in the destination image
-                    dx, dy = destination_image_index
-                    # The affine transformation to be used
-                    k = transformation.name
-                    # Whether the color of the image was inverted or not
-                    c = transformation.name == 'Color Shift'
+                    # Optimization - If the correspondence is 0, e.g. all black blocks, stop the search/return
+                    if int(correspondence) == 0:
+                        best_found = True
+                        break
 
-                    best_candidate = _FractalCandidate(rmse=rmse, fractal=((sx, sy), (dx, dy), k, c))
+            if best_found:
+                break
+
+        self._cache_candidate(destination_block, best_candidate)
 
         return best_candidate
+
+    def _get_cached_candidate(self, block):
+        # Use the string representation of the Numpy array as the key
+        # Reference: https://stackoverflow.com/a/16592241
+        key = hash(block.tostring())
+
+        if key in self._cached_candidates:
+            return self._cached_candidates[key]
+
+        return None
+
+    def _cache_candidate(self, block, candidate):
+        s_block, _, transformation = candidate.fractal
+        self._cached_candidates[hash(block.tostring())] = (candidate.correspondence, s_block, transformation)
 
     def _to_features(self, fractals):
         # Generate the set of features given a list of fractals
         features = set()
 
         for fractal in fractals:
-            source_position, destination_position, transformation, color_shift = fractal
+            source_position, destination_position, transformation = fractal
 
-            # A specific feature
-            features.add(fractal)
-            # A position agnostic feature
-            features.add(((source_position[0] - destination_position[0], source_position[1] - destination_position[1]),
-                          transformation, color_shift))
-            # An affine transformation agnostic feature
-            features.add((source_position, destination_position, color_shift))
-            # A color agnostic feature
-            features.add((source_position, destination_position, transformation))
-            # An affine specific feature
-            features.add((transformation, color_shift)),
-            # A color shift specific feature
-            features.add(color_shift)
+            features.update([
+                # A specific feature
+                fractal,
+                # A position agnostic feature
+                ((source_position[0] - destination_position[0], source_position[1] - destination_position[1]),
+                 transformation),
+                # An affine transformation agnostic feature
+                (source_position, destination_position),
+                # A color agnostic feature
+                (source_position, destination_position, transformation),
+                # An affine specific feature
+                transformation
+            ])
 
         return features
 
-    def _similarity(self, image, other_image):
-        # Computes the similarity between this image and another one using the Normalized Root Mean Squared Error
-        # References:
-        # - https://www.pyimagesearch.com/2014/09/15/python-compare-two-images/
-        # - https://tinyurl.com/ybmwlsur
-        rmse = np.sqrt(np.mean((image - other_image) ** 2))
-        max_val = max(np.max(image), np.max(other_image))
-        min_val = min(np.min(image), np.min(other_image))
+    def _decompose(self, array, size):
+        # Generates a list of `size x size` blocks following a sliding window with stride 1
+        # Reference: https://realpython.com/numpy-array-programming/#image-feature-extraction
+        return stride_tricks.as_strided(
+            array,
+            shape=(array.shape[0] - size + 1, array.shape[1] - size + 1, size, size),
+            strides=2 * array.strides
+        ).reshape(-1, size, size)
 
-        # `max_val - min_val == 0` happens when the two images are either all black or all white
-        return 1 - rmse if max_val - min_val == 0 else 1 - (rmse / (max_val - min_val))
+    def _correspondence(self, image, other_image):
+        # Computes the correspondence between `image` and `other_image`
+        # Photometric correspondence, i.e. square error and distance
+        # For distance, see:
+        # https://stackoverflow.com/questions/1401712/how-can-the-euclidean-distance-be-calculated-with-numpy
+        return (self._PHOTOMETRIC_WEIGHT * np.sum((other_image - image) ** 2) +
+                self._DISTANCE_WEIGHT * np.linalg.norm(other_image - image))
