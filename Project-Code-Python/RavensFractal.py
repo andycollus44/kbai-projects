7e9f8a256a4608d8eb7beee7c44f5faa4b3cd9bf
Allan Reyes <reyallan@gatech.edu>
2018-10-21 18:35:39 -0700

    Handle all-white images in RavensFractalEncoder
    - If the destination image is all white, no encoding is needed
    - If the source image is all white, incorporate the color contraction
      operation as defined by McGreggor. This also expans the feature
      creation with the new color shift characteristic

--- Project-Code-Python/RavensFractal.py
+++ Project-Code-Python/RavensFractal.py
@@ -1,4 +1,3 @@
-import time
 from collections import namedtuple
 
 import numpy as np
@@ -103,6 +102,11 @@ class RavensFractalEncoder:
     _PHOTOMETRIC_WEIGHT = 0.8
     _DISTANCE_WEIGHT = 0.2
 
+    _COLOR_CONTRACTION_DILATION = 0.75
+
+    # The value for a white pixel
+    _WHITE = 255
+
     def __init__(self):
         # The list of affine transformations to apply as described in McGreggor's dissertation
         # This transformations are vectorized to work with a stack of `size x size` blocks
@@ -140,6 +144,10 @@ class RavensFractalEncoder:
         # The cache is only valid per run
         self._cached_candidates.clear()
 
+        # Optimization - If the destination image is all white, then no encoding is needed!
+        if np.alltrue(destination == self._WHITE):
+            return [], set()
+
         # Decompose the destination and source into a set of N smaller images of size `size x size`
         # This will be done in a sliding window fashion with a step size, or stride, of 1 and blocks of `size x size`
         N = destination.shape[0] - size + 1
@@ -154,7 +162,7 @@ class RavensFractalEncoder:
         fractals = [
             self._find_best_candidate(destination_blocks[i], i, source_blocks, block_indices).fractal
             for i in range(0, len(block_indices))
-            if not np.alltrue(destination_blocks[i] == 255)
+            if not np.alltrue(destination_blocks[i] == self._WHITE)
         ]
 
         return fractals, self._to_features(fractals)
@@ -168,7 +176,8 @@ class RavensFractalEncoder:
         if cached_candidate is not None:
             return _FractalCandidate(correspondence=cached_candidate[0], fractal=(cached_candidate[1],
                                                                                   block_indices[destination_index],
-                                                                                  cached_candidate[2]))
+                                                                                  cached_candidate[2],
+                                                                                  cached_candidate[3]))
 
         best_candidate = None
         best_found = False
@@ -180,12 +189,13 @@ class RavensFractalEncoder:
             for s_index, s_block in enumerate(transformed_blocks):
                 correspondence = self._correspondence(destination_block, s_block)
 
+                # Only keep the best candidate
                 if not best_candidate or correspondence < best_candidate.correspondence:
-                    # Only keep the best candidate
                     best_candidate = _FractalCandidate(correspondence=correspondence,
                                                        fractal=(block_indices[s_index],
                                                                 block_indices[destination_index],
-                                                                transformation.name))
+                                                                transformation.name,
+                                                                self._color_contraction(s_block, destination_block)))
 
                     # Optimization - If the correspondence is 0, e.g. all black blocks, stop the search/return
                     if int(correspondence) == 0:
@@ -210,28 +220,31 @@ class RavensFractalEncoder:
         return None
 
     def _cache_candidate(self, block, candidate):
-        s_block, _, transformation = candidate.fractal
-        self._cached_candidates[hash(block.tostring())] = (candidate.correspondence, s_block, transformation)
+        s_block, _, transformation, color_contraction = candidate.fractal
+        self._cached_candidates[hash(block.tostring())] = (candidate.correspondence, s_block, transformation,
+                                                           color_contraction)
 
     def _to_features(self, fractals):
-        # Generate the set of features given a list of fractals
+        # Generate the set of features given a list of fractals as described by McGreggor, Kunda and Goel (2010)
         features = set()
 
         for fractal in fractals:
-            source_position, destination_position, transformation = fractal
+            source_position, destination_position, transformation, color_contraction = fractal
 
             features.update([
                 # A specific feature
                 fractal,
                 # A position agnostic feature
                 ((source_position[0] - destination_position[0], source_position[1] - destination_position[1]),
-                 transformation),
+                 transformation, color_contraction),
                 # An affine transformation agnostic feature
-                (source_position, destination_position),
+                (source_position, destination_position, color_contraction),
                 # A color agnostic feature
                 (source_position, destination_position, transformation),
                 # An affine specific feature
-                transformation
+                (transformation, color_contraction),
+                # A color shift specific feature
+                color_contraction
             ])
 
         return features
@@ -252,3 +265,8 @@ class RavensFractalEncoder:
         # https://stackoverflow.com/questions/1401712/how-can-the-euclidean-distance-be-calculated-with-numpy
         return (self._PHOTOMETRIC_WEIGHT * np.sum((other_image - image) ** 2) +
                 self._DISTANCE_WEIGHT * np.linalg.norm(other_image - image))
+
+    def _color_contraction(self, source, destination):
+        # Computes the color contraction, i.e. the rate at which two regions may be brought into colorimetric harmony,
+        # as described my McGreggor in his dissertation
+        return self._COLOR_CONTRACTION_DILATION * (np.mean(destination) - np.mean(source))
